d20ebc979e5d7d4c4c5db3f90fcacdc86700284a
S <sups@softwareag.com>
2023-11-21 23:37:02 +0530

    Approve Review action yaml file

--- .github/actions/ApproveReview/dist/index.js
+++ .github/actions/ApproveReview/dist/index.js
@@ -32,30 +32,61 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core_1 = __nccwpck_require__(2186);
 const github = __importStar(__nccwpck_require__(5438));
-async function createPullRequest() {
+// async function createPullRequest() {
+//   const octokit=github.getOctokit(process.env.TOKEN_PAT as string)
+//   try {
+//     const owner = github.context.repo.owner;
+//     const repo = github.context.repo.repo;
+//     const baseBranch = getInput('base-branch') || github.context.ref;
+//     const headBranch = getInput('main-branch') || 'main';
+//     const title = getInput('pull-request-title');
+//     const body = getInput('pull-request-body');
+//     const pullRequest = await octokit.rest.pulls.create({
+//       owner,
+//       repo,
+//       title,
+//       body,
+//       base: baseBranch,
+//       head: headBranch,
+//     });
+//     setOutput('pull-request-url', pullRequest.data.html_url);
+//   } catch (error) {
+//     setFailed((error as Error).message);
+//   }
+// }
+// createPullRequest();
+async function createIssueAndComment() {
     const octokit = github.getOctokit(process.env.TOKEN_PAT);
     try {
         const owner = github.context.repo.owner;
         const repo = github.context.repo.repo;
-        const baseBranch = (0, core_1.getInput)('base-branch') || github.context.ref;
-        const headBranch = (0, core_1.getInput)('main-branch') || 'main';
-        const title = (0, core_1.getInput)('pull-request-title');
-        const body = (0, core_1.getInput)('pull-request-body');
-        const pullRequest = await octokit.rest.pulls.create({
+        const title = `Cycle Status ${github.context.runNumber} - ${github.context.runId}: [NOGO] - Review Needed `;
+        const body = `${github.context.job} failed in the ${github.context.workflow} workflow. The status is NOGO`;
+        // Create the issue
+        const issue = await octokit.rest.issues.create({
             owner,
             repo,
             title,
             body,
-            base: baseBranch,
-            head: headBranch,
         });
-        (0, core_1.setOutput)('pull-request-url', pullRequest.data.html_url);
+        const commentBody = "@SAG-Trial/teams-1 Please review and approve the cycle run";
+        const comment = (0, core_1.getInput)("comment-body");
+        // Add a comment to the issue
+        await octokit.rest.issues.createComment({
+            owner,
+            repo,
+            issue_number: issue.data.number,
+            body: commentBody,
+        });
     }
     catch (error) {
         (0, core_1.setFailed)(error.message);
     }
 }
-createPullRequest();
+createIssueAndComment();
+// If you enable email or web notifications for GitHub Actions, you'll receive a notification when any workflow runs that you've triggered have completed. The notification will include the workflow run's status (including successful, failed, neutral, and canceled runs). You can also choose to receive a notification only when a workflow run has failed. For more information about enabling or disabling notifications, see "About notifications."
+// Notifications for scheduled workflows are sent to the user who initially created the workflow. If a different user updates the cron syntax in the workflow file, subsequent notifications will be sent to that user instead. If a scheduled workflow is disabled and then re-enabled, notifications will be sent to the user who re-enabled the workflow rather than the user who last modified the cron syntax.
+// You can also see the status of workflow runs on a repository's Actions tab. For more information, see "Managing workflow runs."
 
 
 /***/ }),
